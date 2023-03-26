ea6cda37c4c6d3b990c2f031d2b28bf8cef2bb04
tk-home-desk <info@v-tk.com>
2023-03-26 13:41:18 +0900

    GATE 設定　迄

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role' // User::create()でまとめて登録できるようにするため
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::prefix('manager')
+->middleware('can:manager-higher')->group(function(){
+ Route::get('index', function () {
+  dd('manager');
+ });
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
