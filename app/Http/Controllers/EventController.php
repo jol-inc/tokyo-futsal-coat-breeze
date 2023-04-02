2e9fbe784afba0bae2abda965e578bb416c4b8c1
tk-home-desk <info@v-tk.com>
2023-04-02 17:31:41 +0900

    コートレンタルをキャンセルした物は非表示

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -53,6 +53,7 @@ class EventController extends Controller
       $join->on('events.id', '=', 'reservedPeople.event_id');
       })
       ->where('events.is_visible',true)//表示中のイベント
+      ->whereNull('canceled_date')//コートレンタルをキャンセルした物は非表示
       ->whereBetween('start_date', [$startDate, $endDate])
       ->orderBy('start_date', 'asc')
       ->get();
@@ -106,6 +107,7 @@ class EventController extends Controller
       $join->on('events.id', '=', 'reservedPeople.event_id');
       })
       ->where('is_visible',true)//表示中にした物
+      ->whereNull('canceled_date')//コートレンタルをキャンセルした物は非表示
       ->whereBetween('start_date', [$startDate, $endDate])
       ->orderBy('start_date', 'asc')
       ->get();
