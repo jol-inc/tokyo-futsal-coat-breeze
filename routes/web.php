1adcf2184e49feb07d4028d3125c9c0c5de3e841
tk-home-desk <info@v-tk.com>
2023-04-25 10:49:00 +0900

    プロフィール　非表示　web.php

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::middleware('auth','can:manager-higher')
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
