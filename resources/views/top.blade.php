472a9a91455f96e8b8b54baa5cfe667c845f9918
tk-home-desk <info@v-tk.com>
2023-05-17 15:12:42 +0900

    TOPページ　注意書き　カラー

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -13,7 +13,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                  <div class="text-2xl">
+                  <div class="text-2xl text-blue-500 mt-12">
                     <p>～お客様アカウントで出来る事～<br>
                     <p>▼コートレンタル<br>
                     <p>・カレンダーでコートの空き確認 → コートレンタルページから予約</p>
@@ -24,15 +24,16 @@
                     <p>・予約の取消</p>
                   </div>
 
-                  <div class="text-blue-500 mt-12 text-2xl">
+                  <div class="text-2xl text-red-500 mt-8 mb-8">
                     <p>～店舗管理者アカウントで出来る事～</p>
                     <p>▼店側主催の各種イベントを作成</p>
                     <p>・店舗管理者ページ　→　イベント新規登録ページ</p>
                     <p>▼（店舗主催イベント、お客様コートレンタル）の一覧、詳細</p>
                     <p>▼便宜上、店舗管理者アカウントもお客様として使える仕様です</p>
                   </div>
+                  
 
-                  <div class="text-red-500 mt-12 text-2xl">
+                  <div class="text-2xl">
                     <p>～その他仕様説明～</p>
                     <p>▼ユーザー登録時のメール認証は省略</p>
                     <p>▼本日のイベントは時刻が過ぎていても、編集、削除可能</p>
